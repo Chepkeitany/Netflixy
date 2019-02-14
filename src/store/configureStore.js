44b0ee6686dd6b671782d3956e7425a6da56e03f
Anne Chepkeitany <rerimoianne.4@gmail.com>
2019-02-14 11:36:55 +0300

    Configure Hot Module Realoding using webpack's module.hot.accept to reload whenever there are changes to our rootReducer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,17 @@
 import { createStore, applyMiddleware} from "redux";
 import logger from "redux-logger";
 import rootReducer from "../reducers";
+import api from "../middleware/api";
 
 const configureStore = initialState => {
-    const store = createStore(rootReducer, initialState, applyMiddleware(logger));
+    const store = createStore(rootReducer, initialState, applyMiddleware(api, logger));
+
+    if (module.hot) {
+        // Enable Webpack hot module replacement for reducers
+        module.hot.accept('../reducers', () => {
+            store.replaceReducer(rootReducer)
+        });
+    };
     return store;
 }
 
