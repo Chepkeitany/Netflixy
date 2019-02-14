b89fd5ed9c331c7df4983433b25dd8930ecbcbf1
Anne Chepkeitany <rerimoianne.4@gmail.com>
2019-02-14 13:26:08 +0300

    Add prettier, StyledMovieContainer

---
+++ src/utils/styledUtils.js
@@ -0,0 +1,27 @@
+import { css } from "styled-components";
+// sm -> small devices
+// md -> medium devices
+// lg -> large devices
+// xl -> extra large devices
+export const devices = {
+  sm: (...args) => css`
+    @media (min-width: 576px) {
+      ${css(...args)}
+    }
+  `,
+  md: (...args) => css`
+    @media (min-width: 768px) {
+      ${css(...args)}
+    }
+  `,
+  lg: (...args) => css`
+    @media (min-width: 992px) {
+      ${css(...args)}
+    }
+  `,
+  xl: (...args) => css`
+    @media (min-width: 1200px) {
+      ${css(...args)}
+    }
+  `
+};
