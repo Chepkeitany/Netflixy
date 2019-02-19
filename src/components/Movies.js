94f88f236e259cacd0bc5006ea43b0dead3bba3f
Anne Chepkeitany <rerimoianne.4@gmail.com>
2019-02-19 09:41:07 +0300

    Include Movie component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import FontAwesomeIcon from "./FontAwesomeIcon";
 import StyledHeader from "./StyledHeader";
 import StyledHeaderTitle from "./StyledHeaderTitle";
+import StyledMovie from "./StyledMovie";
+import StyledPoster from "./StyledPoster";
+import StyledMovieTitle from "./StyledMovieTitle";
+import StyledMovieLengthYear from "./StyledMovieLengthYear";
 
-const Movies = () => (
-  <StyledHeader>
-    <FontAwesomeIcon icon="bars" text="help" />
-    <StyledHeaderTitle> The Movie Recommender </StyledHeaderTitle>
-    <FontAwesomeIcon icon="search" />
-  </StyledHeader>
+const Movies = ({ poster, name, duration, year }) => (
+  <Fragment>
+    <StyledHeader>
+      <FontAwesomeIcon icon="bars" text="help" />
+      <StyledHeaderTitle> The Movie Recommender </StyledHeaderTitle>
+      <FontAwesomeIcon icon="search" />
+    </StyledHeader>
+    <StyledMovie>
+      <StyledPoster src={poster} alt={`Movie: ${name}`} />
+      <StyledMovieTitle>{name}</StyledMovieTitle>
+      <StyledMovieLengthYear>{`${duration} ${year}`}</StyledMovieLengthYear>
+    </StyledMovie>
+  </Fragment>
 );
 
 export default Movies;
