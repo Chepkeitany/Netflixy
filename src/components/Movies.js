ea14593f0791af1ff506166384b57e919a7e44c8
Anne Chepkeitany <rerimoianne.4@gmail.com>
2019-02-19 09:49:21 +0300

    Update StyledHorizontalScroll

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,23 +2,15 @@ import React, { Fragment } from "react";
 import FontAwesomeIcon from "./FontAwesomeIcon";
 import StyledHeader from "./StyledHeader";
 import StyledHeaderTitle from "./StyledHeaderTitle";
-import StyledMovie from "./StyledMovie";
-import StyledPoster from "./StyledPoster";
-import StyledMovieTitle from "./StyledMovieTitle";
-import StyledMovieLengthYear from "./StyledMovieLengthYear";
+import Movie from "./Movie";
 
-const Movies = ({ poster, name, duration, year }) => (
+const Movies = () => (
   <Fragment>
     <StyledHeader>
       <FontAwesomeIcon icon="bars" text="help" />
       <StyledHeaderTitle> The Movie Recommender </StyledHeaderTitle>
       <FontAwesomeIcon icon="search" />
     </StyledHeader>
-    <StyledMovie>
-      <StyledPoster src={poster} alt={`Movie: ${name}`} />
-      <StyledMovieTitle>{name}</StyledMovieTitle>
-      <StyledMovieLengthYear>{`${duration} ${year}`}</StyledMovieLengthYear>
-    </StyledMovie>
   </Fragment>
 );
 
